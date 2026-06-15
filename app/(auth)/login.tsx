d6d1a368deffb2ded864f97ee1d937a3b1717f80
Jolomi Taiwo <taiwojolomi15@gmail.com>
2026-06-16 00:21:32 +0100

    login comp -- revert this

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -2,24 +2,87 @@ import { COLORS } from "@/constants/theme";
 import { styles } from "@/styles/auth.styles";
 import { useSSO } from "@clerk/expo";
 import { Ionicons } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import * as AuthSession from "expo-auth-session";
+import { type Href, useRouter } from "expo-router";
+import * as WebBrowser from "expo-web-browser";
+import * as React from "react";
+import { Image, Platform, Text, TouchableOpacity, View } from "react-native";
+
+// Preloads the browser for Android devices to reduce authentication load time
+// See: https://docs.expo.dev/guides/authentication/#improving-user-experience
+export const useWarmUpBrowser = () => {
+	React.useEffect(() => {
+		if (Platform.OS !== "android") return;
+		void WebBrowser.warmUpAsync();
+		return () => {
+			void WebBrowser.coolDownAsync();
+		};
+	}, []);
+};
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function login() {
+	useWarmUpBrowser();
+
 	const { startSSOFlow } = useSSO();
 	const router = useRouter();
 	const handleGoogleSignIn = async () => {
+		// try {
+		// 	const { createdSessionId, setActive } = await startSSOFlow({
+		// 		strategy: "oauth_google",
+		// 		redirectUrl: AuthSession.makeRedirectUri({
+		// 			path: "/app",
+		// 		}),
+		// 	});
+
+		// 	if (setActive && createdSessionId) {
+		// 		console.log("Authenticated", createdSessionId);
+		// 		setActive({ session: createdSessionId });
+		// 		router.replace("/(tabs)");
+		// 	}
+		// } catch (error) {
+		// 	console.log("OAuth Error", error);
+		// }
 		try {
 			const { createdSessionId, setActive } = await startSSOFlow({
 				strategy: "oauth_google",
+				// For web, defaults to current path
+				// For native, you must pass a scheme, like AuthSession.makeRedirectUri({ scheme, path })
+				// For more info, see https://docs.expo.dev/versions/latest/sdk/auth-session/#authsessionmakeredirecturioptions
+				redirectUrl: AuthSession.makeRedirectUri({
+					scheme: "clerkexpoquickstart",
+					path: "exp://192.168.1.8:8081/(tabs)",
+				}),
 			});
 
-			if (setActive && createdSessionId) {
-				setActive({ session: createdSessionId });
-				router.replace("/(tabs)");
+			// If the session was created, set it as the active session
+			if (createdSessionId) {
+				setActive!({
+					session: createdSessionId,
+					navigate: async ({ session, decorateUrl }) => {
+						// Handle session tasks
+						// See https://clerk.com/docs/guides/development/custom-flows/authentication/session-tasks
+						if (session?.currentTask) {
+							console.log(session?.currentTask);
+							return;
+						}
+
+						// If no session tasks, navigate the signed-in user to the home page
+						const url = decorateUrl("/");
+						if (url.startsWith("http")) {
+							window.location.href = url;
+						} else {
+							router.push(url as Href);
+						}
+					},
+				});
+			} else {
+				// If the session was not created, navigate to the continue page to collect missing information
+				router.push("/(auth)/login");
 			}
-		} catch (error) {
-			console.log("OAuth Error", error);
+		} catch (err) {
+			console.error(JSON.stringify(err, null, 2));
 		}
 	};
 	return (
