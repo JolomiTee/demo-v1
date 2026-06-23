fd3d5a7261b0d93787e41ef643a0853f3823c079
Jolomi Taiwo <taiwojolomi15@gmail.com>
2026-06-23 15:13:53 +0100

    - added font to the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,20 @@
 import ClerkConvexProviders from "@/components/providers/Clerk_Convex_Providers";
-import { Slot, usePathname } from "expo-router";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen, usePathname } from "expo-router";
+import { useCallback } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
 	const pathname = usePathname();
+	const [fontsLoaded] = useFonts({
+		"JetBrainsMono-Medium": require("@/assets/fonts/JetBrainsMono-Medium.ttf"),
+	});
+
+	const onLayoutRootView = useCallback(async () => {
+		if (fontsLoaded) SplashScreen.hideAsync();
+	}, [fontsLoaded]);
 
 	console.log(pathname);
 	return (
@@ -14,6 +25,7 @@ export default function RootLayout() {
 						flex: 1,
 						backgroundColor: "black",
 					}}
+					onLayout={onLayoutRootView}
 				>
 					<Slot />
 				</SafeAreaView>
